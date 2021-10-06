78de21943d6757b326af59c0f70e20cf11caa7c8
jvredmoore <jvredmoore@gmail.com>
2021-10-06 14:47:10 +0000

    worked on code challenge

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //1
-function absoluteValue(){
-  
+function absoluteValue(number){
+  console.log(typeof number)
+  if(typeof number === "number"){
+    if(number < 0){
+        return number * -1
+    }  
+  }else{
+     return number
+    }
+  }else{
+      return null
 }
 
+absoluteValue(20)
 
 
 
